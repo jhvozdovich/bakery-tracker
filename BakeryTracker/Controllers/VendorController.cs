294386844f6eff5c84324245e3f9599ad3d65f9e
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-05-22 10:46:49 -0700

    add initial order detail views and controllers

--- BakeryTracker/Controllers/VendorController.cs
+++ BakeryTracker/Controllers/VendorController.cs
@@ -25,5 +25,28 @@ namespace BakeryTracker.Controllers
       Vendor newVendor = new Vendor(name, address);
       return RedirectToAction("Index");
     }
+
+    // [HttpGet("/vendors/{vendorid}")]
+    // public ActionResult Show(int id)
+    // {
+    //   Dictionary<string, object> model = new Dictionary<string, object>();
+    //   Vendor vendor = Vendor.Find(id);
+    //   List<Order> vendorOrders = vendor.Orders;
+    //   model.Add("vendor", vendor);
+    //   model.Add("orders", vendorOrders);
+    //   return View(model);
+    // }
+
+    // [HttpPost("/vendors/{vendorid}/orders")]
+    // public ActionResult Create(int vendorId, string type, int quantity)
+    // {
+    //   Dictionary<string, object> model = new Dictionary<string, object>();
+    //   Vendor vendor = Vendor.Find(vendorId);
+    //   Order newOrder = new Order(type, quantity);
+    //   List<Order> vendorOrders = vendor.Orders;
+    //   model.Add("vendor", vendor);
+    //   model.Add("orders", vendorOrders);
+    //   return View("Show", model);
+    // }
   }
 }

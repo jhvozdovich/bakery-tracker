a91cbe15b015bc52f146a33a9cbf23ba088bac48
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-05-22 14:33:11 -0700

    add test for GetDescription with meaningful failure

--- BakeryTracker.Tests/ModelTests/OrderTests.cs
+++ BakeryTracker.Tests/ModelTests/OrderTests.cs
@@ -16,7 +16,7 @@ namespace BakeryTracker.Tests
     [TestMethod]
     public void OrderConstructor_CreatesOrderInstance_Object()
     {
-      Order newOrder = new Order("Baguette", 2);
+      Order newOrder = new Order("Baguette", 2, "delicous crust", 9, "November 8");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
 
@@ -25,7 +25,10 @@ namespace BakeryTracker.Tests
     {
       string type = "Baguette";
       int quantity = 2;
-      Order newOrder = new Order(type, quantity);
+      string description = "delicious crust";
+      double price = 9;
+      string dueDate = "November 8";
+      Order newOrder = new Order(type, quantity, description, price, dueDate);
       string result = newOrder.Type;
       Assert.AreEqual(type, result);
     }
@@ -35,9 +38,25 @@ namespace BakeryTracker.Tests
     {
       string type = "Baguette";
       int quantity = 2;
-      Order newOrder = new Order(type, quantity);
+      string description = "delicious crust";
+      double price = 9;
+      string dueDate = "November 8";
+      Order newOrder = new Order(type, quantity, description, price, dueDate);
       int result = newOrder.Quantity;
       Assert.AreEqual(quantity, result);
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      string type = "Baguette";
+      int quantity = 2;
+      string description = "delicious crust";
+      double price = 9;
+      string dueDate = "November 8";
+      Order newOrder = new Order(type, quantity, description, price, dueDate);
+      string result = newOrder.Description;
+      Assert.AreEqual(type, result);
     }
 
     [TestMethod]
@@ -45,13 +64,22 @@ namespace BakeryTracker.Tests
     {
       string type1 = "Baguette";
       int quantity1 = 2;
+      string description1 = "delicious crust";
+      double price1 = 9;
+      string dueDate1 = "November 8";
       string type2 = "Croissant";
       int quantity2 = 10;
+      string description2 = "soft and flakey";
+      double price2 = 3;
+      string dueDate2 = "November 11";
       string type3 = "Brioche";
       int quantity3 = 4;
-      Order newOrder1 = new Order(type1, quantity1);
-      Order newOrder2 = new Order(type2, quantity2);
-      Order newOrder3 = new Order(type3, quantity3);
+      string description3 = "like a little cloud";
+      double price3 = 4;
+      string dueDate3 = "November 10";
+      Order newOrder1 = new Order(type1, quantity1, description1, price1, dueDate1);
+      Order newOrder2 = new Order(type2, quantity2, description2, price2, dueDate2);
+      Order newOrder3 = new Order(type3, quantity3, description3, price3, dueDate3);
       int result = newOrder3.Id;
       Assert.AreEqual(3, result);
     }
@@ -61,13 +89,22 @@ namespace BakeryTracker.Tests
     {
       string type1 = "Baguette";
       int quantity1 = 2;
+      string description1 = "delicious crust";
+      double price1 = 9;
+      string dueDate1 = "November 8";
       string type2 = "Croissant";
       int quantity2 = 10;
+      string description2 = "soft and flakey";
+      double price2 = 3;
+      string dueDate2 = "November 11";
       string type3 = "Brioche";
       int quantity3 = 4;
-      Order newOrder1 = new Order(type1, quantity1);
-      Order newOrder2 = new Order(type2, quantity2);
-      Order newOrder3 = new Order(type3, quantity3);
+      string description3 = "like a little cloud";
+      double price3 = 4;
+      string dueDate3 = "November 10";
+      Order newOrder1 = new Order(type1, quantity1, description1, price1, dueDate1);
+      Order newOrder2 = new Order(type2, quantity2, description2, price2, dueDate2);
+      Order newOrder3 = new Order(type3, quantity3, description3, price3, dueDate3);
       int beforeClear = Order.GetAll().Count;
       Assert.AreEqual(3, beforeClear);
       Order.ClearAll();
@@ -80,13 +117,22 @@ namespace BakeryTracker.Tests
     {
       string type1 = "Baguette";
       int quantity1 = 2;
+      string description1 = "delicious crust";
+      double price1 = 9;
+      string dueDate1 = "November 8";
       string type2 = "Croissant";
       int quantity2 = 10;
+      string description2 = "soft and flakey";
+      double price2 = 3;
+      string dueDate2 = "November 11";
       string type3 = "Brioche";
       int quantity3 = 4;
-      Order newOrder1 = new Order(type1, quantity1);
-      Order newOrder2 = new Order(type2, quantity2);
-      Order newOrder3 = new Order(type3, quantity3);
+      string description3 = "like a little cloud";
+      double price3 = 4;
+      string dueDate3 = "November 10";
+      Order newOrder1 = new Order(type1, quantity1, description1, price1, dueDate1);
+      Order newOrder2 = new Order(type2, quantity2, description2, price2, dueDate2);
+      Order newOrder3 = new Order(type3, quantity3, description3, price3, dueDate3);
       List<Order> newList = new List<Order> { newOrder1, newOrder2, newOrder3 };
       List<Order> result = Order.GetAll();
       CollectionAssert.AreEqual(newList, result);
@@ -97,13 +143,22 @@ namespace BakeryTracker.Tests
     {
       string type1 = "Baguette";
       int quantity1 = 2;
+      string description1 = "delicious crust";
+      double price1 = 9;
+      string dueDate1 = "November 8";
       string type2 = "Croissant";
       int quantity2 = 10;
+      string description2 = "soft and flakey";
+      double price2 = 3;
+      string dueDate2 = "November 11";
       string type3 = "Brioche";
       int quantity3 = 4;
-      Order newOrder1 = new Order(type1, quantity1);
-      Order newOrder2 = new Order(type2, quantity2);
-      Order newOrder3 = new Order(type3, quantity3);
+      string description3 = "like a little cloud";
+      double price3 = 4;
+      string dueDate3 = "November 10";
+      Order newOrder1 = new Order(type1, quantity1, description1, price1, dueDate1);
+      Order newOrder2 = new Order(type2, quantity2, description2, price2, dueDate2);
+      Order newOrder3 = new Order(type3, quantity3, description3, price3, dueDate3);
       Order result = Order.Find(2);
       Assert.AreEqual(result, newOrder2);
     }

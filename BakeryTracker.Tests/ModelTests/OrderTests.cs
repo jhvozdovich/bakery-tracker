09db771ad7b2b6ff2f7bae028eaed98513336c91
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-05-22 11:03:52 -0700

    update order instance test, pass

--- BakeryTracker.Tests/ModelTests/OrderTests.cs
+++ BakeryTracker.Tests/ModelTests/OrderTests.cs
@@ -17,7 +17,7 @@ namespace BakeryTracker.Tests
     public void OrderConstructor_CreatesOrderInstance_Object()
     {
       Order newOrder = new Order("Baguette", 2);
-      Assert.AreEqual(0, newOrder.GetType());
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
   }
 }
